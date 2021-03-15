9456bae151efd20f03e93a9174f3fb961d35a8ca
alexis <alexissaborrido.ece@gmail.com>
2021-03-15 23:40:27 +0800

    moved partials for php

---
+++ partials/topbar.php
@@ -0,0 +1,9 @@
+<!-- Top Bar -->
+<div class="top-bar">
+    <div class="container">
+        <div class="col-12 text-right">
+            <p><a href="https://www.facebook.com/AnyColourYouLiek/" target="_blank">Reach me here!!!</a></p>
+        </div>
+    </div>
+</div>
+<!-- End Top Bar -->
